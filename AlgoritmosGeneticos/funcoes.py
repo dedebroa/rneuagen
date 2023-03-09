bf8548c0656c8d692c20d8784d9ed9d90f7a0617
Débora Van Putten Chaves <debora220037@ilum.cnpem.br>
2023-03-09 17:12:57 -0300

    Add Experimento 1, 2 e funcoes.py

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,12 @@
- 
+def funcao_objetivo(individuo):
+    '''Computa a função objetivo para o problema cb.
+    
+        Argumentos:
+            Individuo: lista contendo os genes das caixas binárias
+        
+        Return:
+            Um valor representando a soma dos genes do indivíduo
+    '''
+    return sum(individuo) 
+
+# lembrar que aqui não tem célula de texto, comentários precisam ter hashtag
