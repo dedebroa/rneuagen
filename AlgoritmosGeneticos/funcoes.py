ddfeacbd4a5529c5076b73654e88400a78dfaa23
Débora Van Putten Chaves <debora220037@ilum.cnpem.br>
2023-03-16 14:53:37 -0300

    atualizando arquivo funções e todos os experimentos

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,15 +1,4 @@
-def funcao_objetivo(individuo):
-    '''Computa a função objetivo para o problema cb.
-    
-        Argumentos:
-            Individuo: lista contendo os genes das caixas binárias
-        
-        Return:
-            Um valor representando a soma dos genes do indivíduo
-    '''
-    return sum(individuo) 
-
-#lembrar que aqui não tem célula de texto, comentários precisam ter hashtag
+import random 
 
 def gene_cb():
     '''Gera um gene válido para o problema das caixas binários 
@@ -20,3 +9,30 @@ def gene_cb():
     lista = [0, 1] # lá no início dissemos que os genes poderiam assumir dois valores
     gene = random.choice(lista) # pedirei que a função escolha aleatóriamnte um valor para o gene dentro da lista
     return gene # peço que ele retorne a escolha aleatória
+
+
+def individuo_cb(n):
+    '''Gera um indivíduo para o problema das caixas binárias (um indivíduo tem 4 genes)
+    
+    Argumentos:
+        n: número de genes do indivíduo (ou seja, número de caixas)
+        
+    Return:
+        Uma lista com n genes (caixas). Cada gene é um valor zero ou um.
+    '''
+    individuo = [] # importante lembrar: o indivíduo corresponde ao conjunto de 4 genes, ou seja, ele será uma lista desses 4 valores
+    for i in range(n):
+        gene = gene_cb()
+        individuo.append(gene)
+    return individuo
+    
+def funcao_objetivo(individuo):
+    '''Computa a função objetivo para o problema cb.
+    
+        Argumentos:
+            Individuo: lista contendo os genes das caixas binárias
+        
+        Return:
+            Um valor representando a soma dos genes do indivíduo
+    '''
+    return sum(individuo)
