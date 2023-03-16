9dab10944c59f03e36385ded6bb1c1ac3b25692c
Débora Van Putten Chaves <debora220037@ilum.cnpem.br>
2023-03-16 14:04:01 -0300

    Atualizando já os experimentos 3 e 4, o que já fiz até agora + o funcoes

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -10,3 +10,13 @@ def funcao_objetivo(individuo):
     return sum(individuo) 
 
 #lembrar que aqui não tem célula de texto, comentários precisam ter hashtag
+
+def gene_cb():
+    '''Gera um gene válido para o problema das caixas binários 
+    
+    Return:
+        Ou retorna zero ou retona um
+    '''
+    lista = [0, 1] # lá no início dissemos que os genes poderiam assumir dois valores
+    gene = random.choice(lista) # pedirei que a função escolha aleatóriamnte um valor para o gene dentro da lista
+    return gene # peço que ele retorne a escolha aleatória
